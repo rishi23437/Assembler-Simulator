700825e9289ecc755af9c6c37f582471df9af3d0
5aha5 <162083187+5aha5@users.noreply.github.com>
2024-03-04 19:22:35 +0530

    Create u_type.py

---
+++ u_type.py
@@ -0,0 +1,13 @@
+import re
+functions_itype = {"lui"  :"0110111",
+                   "auipc":"0010111"
+                }
+
+def u_type(instruction):
+    item = re.split(" |,", instruction)
+
+    imm=sext(string_to_number(item[2]),20)
+    reg=reg_ENCODE[item[1]]
+    op_c=functions_itype[item[0]]
+    
+    return  imm+reg+op_c
