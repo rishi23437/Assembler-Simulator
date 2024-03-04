4800d25b7c1a2487643f3d07600d2dedc216b47f
5aha5 <162083187+5aha5@users.noreply.github.com>
2024-03-04 19:33:08 +0530

    Update u_type.py

--- u_type.py
+++ u_type.py
@@ -5,7 +5,7 @@ functions_itype = {"lui":"0110111",
 def u_type(instruction):
     item = re.split(" |,", instruction)
 
-    imm=sext(string_to_number(item[2]),20)
+    imm=sext(int(item[2]),20)
     reg=reg_ENCODE[item[1]]
     op_c=functions_itype[item[0]]
     
