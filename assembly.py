ceb9efa655a7ac5598e9c4771f875c0aa983d9ef
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 17:00:50 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -1,3 +1,13 @@
+reg_ENCODE  = {'zero': '00000',   'ra': '00001',     'sp': '00010',     'gp': '00011', 
+               'tp': '00100',     't0': '00101',     't1': '00110',     't2': '00111', 
+               's0': '01000',     'fp': '01000',     's1': '01001',     'a0': '01010',     'a1': '01011', 
+               'a2': '01100',     'a3': '01101',     'a4': '01110',     'a5': '01111', 
+               'a6': '10000',     'a7': '10001',     's2': '10010',     's3': '10011', 
+               's4': '10100',     's5': '10101',     's6': '10110',     's7': '10111', 
+               's8': '11000',     's9': '11001',     's10': '11010',    's11': '11011', 
+               't3': '11100',     't4': '11101',     't5': '11110',     't6': '11111'}
+
+
 
 
 with open(r"", 'r') as pointer:
@@ -8,16 +18,7 @@ pc = 0
 
 # while (pc<(len(assembly))):
 
-def string_EXT( length , filler , value ):
-    num = length - len(value)
-    final = filler*num + value
-    return final
 
-def reg_ENCODE( reg_NAME ):
-    reg_NUM = int(reg_NAME[1:])
-    bin_EQ = bin(reg_NUM)[2:]
-    bin_EQ = string_EXT( 5 , "0" , bin_EQ )
-    return bin_EQ
 
 def sext(number, bits):
     """
