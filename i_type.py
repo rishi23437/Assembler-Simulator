3782e5d113ec827cb0ec2ac222c5ebae2c8197dc
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 16:31:57 +0530

    Update i_type.py

--- i_type.py
+++ i_type.py
@@ -1,20 +1,20 @@
 # addi , sltiu , jalr , l{b|h|w|d}
 
-map_I_TYPE = { "addi" : {opcode: "0010011", funct3: "000"} , 
-              "sltiu" : {opcode: "0010011", funct3: "011"} , 
-              "jalr" : {opcode: "1100111", funct3: "000"} , 
-              "lw" : {opcode: "0000011", funct3: "010"} , 
-              "lb" : {opcode: "", funct3: ""} , 
-              "lh" : {opcode: "", funct3: ""} , 
-              "ld" : {opcode: "", funct3: ""} }
+map_I_TYPE = { "addi" : ["0010011", "000"] , 
+              "sltiu" : ["0010011", "011"] , 
+              "jalr" : ["1100111", "000"] , 
+              "lw" : ["0000011","010"] , 
+              "lb" : ["", ""] , 
+              "lh" : ["", ""] , 
+              "ld" : ["", ""] }
 
 def I_TYPE( I_instruction ):
-  imm = I_instruction[4]
-  rs = reg_ENCODE(I_instruction[3])
-  rd = reg_ENCODE(I_instruction[2])
+  imm = I_instruction[3]
+  rs = reg_ENCODE(I_instruction[2])
+  rd = reg_ENCODE(I_instruction[1])
   
-  funct = map_I_TYPE[I_instruction[0]][funct3]
-  opc = map_I_TYPE[I_instruction[0]][opcode]
+  funct = map_I_TYPE[I_instruction[0]][1]
+  opc = map_I_TYPE[I_instruction[0]][0]
   
   encoded = imm + rs + funct + rd + opc
   return encoded
