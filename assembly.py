d2b5301ea7f632c45a429b6d2c6906952e5b38c6
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 16:08:52 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -6,5 +6,19 @@ with open(r"", 'r') as pointer:
 
 pc = 0
 
-while (pc<(len(assembly))):
+# while (pc<(len(assembly))):
+
+def sign_EXT( length , sign , value ):
+    num = length - len(value)
+    final = sign*num + value
+    return final
+
+def reg_ENCODE( reg_NAME ):
+    reg_NUM = int(reg_NAME[1:])
+    bin_EQ = bin(reg_NUM)[2:]
+    bin_EQ = sign_EXT( 5 , 0 , bin_EQ )
+    return bin_EQ
+
+
+
     
