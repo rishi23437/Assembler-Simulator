1152fba68ffc0488728672ce8e11f1ad6d0a042e
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 16:27:54 +0530

    Update i_type.py

--- i_type.py
+++ i_type.py
@@ -1,8 +1,21 @@
 # addi , sltiu , jalr , l{b|h|w|d}
 
-map_I_TYPE = { "addi" : addi , "sltiu" : sltiu , "jalr" : jalr , "lw" : l_type , "lb" : l_type , "lh" : l_type , "ld" : l_type }
+map_I_TYPE = { "addi" : {opcode: "0010011", funct3: "000"} , 
+              "sltiu" : {opcode: "0010011", funct3: "011"} , 
+              "jalr" : {opcode: "1100111", funct3: "000"} , 
+              "lw" : {opcode: "0000011", funct3: "010"} , 
+              "lb" : {opcode: "", funct3: ""} , 
+              "lh" : {opcode: "", funct3: ""} , 
+              "ld" : {opcode: "", funct3: ""} }
 
-def addi( I_instruction ):
-  rd = I_instruction[2]
-  rs = I_instruction[3]
-  imm = 
+def I_TYPE( I_instruction ):
+  imm = I_instruction[4]
+  rs = reg_ENCODE(I_instruction[3])
+  rd = reg_ENCODE(I_instruction[2])
+  
+  funct = map_I_TYPE[I_instruction[0]][funct3]
+  opc = map_I_TYPE[I_instruction[0]][opcode]
+  
+  encoded = imm + rs + funct + rd + opc
+  return encoded
+
