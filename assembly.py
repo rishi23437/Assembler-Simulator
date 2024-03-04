2c5cf923807a9b352c191a05fb053184b1d974f7
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-03-04 16:46:48 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -19,6 +19,37 @@ def reg_ENCODE( reg_NAME ):
     bin_EQ = string_EXT( 5 , "0" , bin_EQ )
     return bin_EQ
 
+def sext(number, bits):
+    """
+    This function first converts the number into binary
+    and then extends its bits to the required amount
+    """
+    if (number<0):
+        sign = -1
+        number = -number
+    else:
+        sign = 1
+    binary = ""
+    while (number>0):
+        binary += f"{number%2}"
+        number = number//2
+    binary = binary[::-1]
+    binary = ('0'*(bits-len(binary)))+binary
+    if (sign == -1):
+        flag=False
+        twosComplement = ""
+        for i in range(len(binary)-1, -1, -1):
+            if (flag):
+                if binary[i]=='1':
+                    twosComplement+='0'
+                else:
+                    twosComplement+='1'
+                continue
+            twosComplement+=binary[i]
+            if (binary[i]=='1'):
+                flag = True 
+        binary = twosComplement[::-1]
+    return binary
 
 
     
