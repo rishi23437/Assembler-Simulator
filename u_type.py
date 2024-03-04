e66520884827b9af66ef56765f259c68df2da9be
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 22:04:04 +0530

    Update u_type.py

--- u_type.py
+++ u_type.py
@@ -2,8 +2,8 @@ functions_itype = {"lui":"0110111",
                    "auipc":"0010111"
                 }
 
-def u_type(instruction):
-    item = re.split(" |,", instruction)
+def u_type(item):
+    #item = re.split(" |,", instruction)  its already split when giving argument to the function
 
     imm=sext(int(item[2]),20)
     reg=reg_ENCODE[item[1]]
