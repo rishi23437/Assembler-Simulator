753cdb986131b073c8f55152dcc6d540ca15a173
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 15:52:48 +0530

    Update i_type.py

--- i_type.py
+++ i_type.py
@@ -1,3 +1,8 @@
 # addi , sltiu , jalr , l{b|h|w|d}
 
+map_I_TYPE = { "addi" : addi , "sltiu" : sltiu , "jalr" : jalr , "lw" : l_type , "lb" : l_type , "lh" : l_type , "ld" : l_type }
 
+def addi( I_instruction ):
+  rd = I_instruction[2]
+  rs = I_instruction[3]
+  imm = 
