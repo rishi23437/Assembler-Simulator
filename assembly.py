7c47accdb5d7d4d4bb87da04a2bc8be93c225f57
rishi-banerji <137902958+rishi-banerji@users.noreply.github.com>
2024-03-04 17:27:09 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -1,3 +1,5 @@
+import re
+
 reg_ENCODE  = {'zero': '00000',   'ra': '00001',     'sp': '00010',     'gp': '00011', 
                'tp': '00100',     't0': '00101',     't1': '00110',     't2': '00111', 
                's0': '01000',     'fp': '01000',     's1': '01001',     'a0': '01010',     'a1': '01011', 
@@ -17,6 +19,9 @@ with open(r"", 'r') as pointer:
 pc = 0
 
 # while (pc<(len(assembly))):
+  
+  #Split the instruction on ' ' and ',' using regex(re) module. items = re.split(' |,', instruction)
+
 
 
 
