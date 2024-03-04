6037aafd0b7fa3aa34cdd89ca82c4ec0795f9a9c
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-04 17:15:29 +0530

    Update i_type.py

--- i_type.py
+++ i_type.py
@@ -10,8 +10,8 @@ map_I_TYPE = { "addi" : ["0010011", "000"] ,
 
 def I_TYPE( I_instruction ):
   imm = I_instruction[3]
-  rs = reg_ENCODE(I_instruction[2])
-  rd = reg_ENCODE(I_instruction[1])
+  rs = reg_ENCODE[I_instruction[2]]
+  rd = reg_ENCODE[I_instruction[1]]
   
   funct = map_I_TYPE[I_instruction[0]][1]
   opc = map_I_TYPE[I_instruction[0]][0]
