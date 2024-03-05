2c7acdbdac167b679b6dda0ed5ced1354913eb70
5aha5 <162083187+5aha5@users.noreply.github.com>
2024-03-05 13:24:14 +0530

    Update u_type.py

--- u_type.py
+++ u_type.py
@@ -1,12 +1,17 @@
 functions_itype = {"lui":"0110111",
-                   "auipc":"0010111"
-                }
+                   "auipc":"0010111"}
 
-def u_type(item):
-    #item = re.split(" |,", instruction)  its already split when giving argument to the function
+def u_type(U_instruction):
+  
+    ''' 
+    U_instruction is a list of string in format :
+    [(Name of u_type function),(destination register),(value of the immediate)]
 
-    imm=sext(int(item[2]),20)
-    reg=reg_ENCODE[item[1]]
-    op_c=functions_itype[item[0]]
+    returns : Above provided U_instrution into 32 bit binary format in a string
+    '''
+  
+    imm = sext(int(U_instruction[2]),20)
+    reg = reg_ENCODE[U_instruction[1]]
+    op_c= functions_itype[U_instruction[0]]
     
     return  imm+reg+op_c
