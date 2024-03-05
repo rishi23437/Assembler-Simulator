f25b2a16cdfd5a27956844ee025a201979873911
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-03-05 12:51:25 +0530

    Update assembly.py

--- assembly.py
+++ assembly.py
@@ -84,6 +84,8 @@ def sext(number, bits):
     and then extends its bits to the required amount
     """
     number = int(number)
+    if (number<-(2**(bits-1)) or number>(2**(bits-1))-1):
+        return "error: overflow detected"
     if (number<0):
         sign = -1
         number = -number
