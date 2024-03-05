c0e1e9c5ee53be6a6f4b3218c11ed3618334d256
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-05 17:31:05 +0530

    Update r_type.py

--- r_type.py
+++ r_type.py
@@ -11,8 +11,13 @@ functions_r = {"add": ["0000000", "000"],                #opcode: [funct7, funct
 
 def R_TYPE(instruction_list):
     operation = instruction_list[0]
-    reg1, reg2, reg3 = reg_ENCODE[instruction_list[1]], reg_ENCODE[instruction_list[2]], reg_ENCODE[instruction_list[3]]
-    
+    reg1 = reg_ENCODE.get(instruction_list[1])
+    reg2 = reg_ENCODE.get(instruction_list[2])
+    reg3 = reg_ENCODE.get(instruction_list[3])
+
+    if (reg1 == None) or (reg2 == None) or (reg3 == None):
+      return "e3"
+      
     output = functions_r[operation][0] + reg3 + reg2 + functions_r[operation][1] + reg1 + "0110011"
 
     return output 
