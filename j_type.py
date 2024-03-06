0c63f562af9eeddcbde1ab2e4ef4ec76fb3bd954
5aha5 <162083187+5aha5@users.noreply.github.com>
2024-03-06 12:07:00 +0530

    Update j_type.py

--- j_type.py
+++ j_type.py
@@ -1,1 +1,16 @@
+functions_jtype={"jal":"0010111"}
 
+def J_TYPE(J_instruction):
+  '''
+  J_instruction is a string in format
+  {(function name),(destination register),(label)}
+
+  returns : 32 bit binary sequence
+  '''
+  
+  imm=sext(int(J_instruction[2],20))
+  reg=reg_ENCODE.get(J_instruction[1])
+  op_code=functions_jtype[J_instruction[0]]
+  imm=imm[0]+imm[10:20]+imm[9]+[1:9]
+         
+  return imm+reg+op_code
