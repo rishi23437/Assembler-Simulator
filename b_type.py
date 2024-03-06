b60e1e55f4140c02d6aa4aa4b96ab14f340d3e86
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-06 18:44:49 +0530

    Update b_type.py

--- b_type.py
+++ b_type.py
@@ -1,31 +1,26 @@
+map_B_TYPE = { "beq" : ["1100011", "000"] , 
+              "bne" : ["1100011", "001"] , 
+              "blt" : ["1100011", "100"] , 
+              "bge" : ["1100011", "101"] , 
+              "bltu" : ["1100011", "110"] , 
+              "bgeu" : ["1100011", "111"] }
 
-map_I_TYPE = {"addi" :   ["0010011", "000"] , 
-              "sltiu" :  ["0010011", "011"] , 
-              "jalr" :   ["1100111", "000"] , 
-              "lw" :     ["0000011","010"] } 
-
-def I_TYPE( I_instruction ):
-  '''argument : list type, instruction
-  returns : string of encoded binary
-  '''
-  rd = reg_ENCODE.get(I_instruction[1])
-  
-  if ((I_instruction[0] != "lw"):
-      imm = sext(int(I_instruction[3]),12)
-      rs = reg_ENCODE.get(I_instruction[2])    
-
-  else:
-      imm = sext(int(I_instruction[2]),12)
-      rs = reg_ENCODE.get(I_instruction[3]) 
-                           
+def B_TYPE( B_instruction ):
+  """
+  B_instruction is a list of the form [[opcode, funct3], rs1, rs2, immediate value not converted in bits]
+  """
+  imm = sext(int(B_instruction[3]),12)
   if (imm == "e1"):
       return imm
     
-  if (rs == None) or (rd == None):
-      return "e3"
-      
-  funct = map_I_TYPE[I_instruction[0]][1]
-  opc = map_I_TYPE[I_instruction[0]][0]
+  rs1 = reg_ENCODE.get(B_instruction[1])
+  rs2 = reg_ENCODE.get(B_instruction[2])
+
+  if (rs1 == None) or (rs2 == None):
+    return "e3"
   
-  encoded = imm + rs + funct + rd + opc   
-  return encoded
+  funct3 = map_B_TYPE[B_instruction[0]][1]
+  opc = map_B_TYPE[B_instruction[0]][0]
+  
+  decoded = imm[0:7] + rs2 + rs1 + funct3 + imm[7:11] + imm[0] + opc
+  return decoded
