71b01e7af5d0968e63324ad49b593742a74a4799
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-08 22:21:09 +0530

    Update Z-recycleBIN.py

--- Z-recycleBIN.py
+++ Z-recycleBIN.py
@@ -33,3 +33,41 @@ def string_to_number(value):
   fin += value
   return int(fin)
 
+
+def label_in_bounds(label):
+    """
+    label: an Integer, in decimal, signed.
+    this function returns e8 if label is out of bounds, else True.
+    """
+    global PC, assembly
+
+    line_to_jump = PC + label
+    if line_to_jump >= 0 and line_to_jump < len(assembly):
+        return True
+    else:
+        return "e8"
+
+
+  //this code not needed anymore !!    
+        #if label is present in instruction_elements, type will be 2nd element of the list(after removing "")
+        #NOTE: Label has not been removed
+        try: 
+            integer = int(instruction_elements[0])
+            instruction_elements = [element for element in instruction_elements if element != ""]
+            type = instruction_elements[1]
+        except: type = instruction_elements[0]
+
+            if type == "jalr":
+                label_num = int(instruction_elements[-1])               #to check if resultant line(to jump to) is out of bounds
+                if label_in_bounds(label_num) == "e8":
+                    output_list.clear()
+                    output_list.append(errorGEN("e8", PC))
+                    break
+
+
+            label_num = int(instruction_elements[-1])              #to check if resultant line(to jump to) is out of bounds
+            if label_in_bounds(label_num) == "e8":
+                output_list.clear()
+                output_list.append(errorGEN("e8", PC))
+                break
+          
