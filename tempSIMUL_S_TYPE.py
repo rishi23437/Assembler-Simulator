02288dbebe557fb45eb3bc1fba81b744bc18fa25
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-25 13:59:15 +0530

    Update tempSIMUL_S_TYPE.py

--- tempSIMUL_S_TYPE.py
+++ tempSIMUL_S_TYPE.py
@@ -1,1 +1,11 @@
+def S_TYPE( line ):
+  # line is 32 bits
 
+  imm = line[0:7] + line[20:25]
+  rs2 = line[7:12]
+  rs1 = line[12:17]
+  funct3 = line[17:20]
+  opcode = line[25:32]
+
+  memory[register[rs1] + bin_to_dec(imm)] = register[rs2]
+    
