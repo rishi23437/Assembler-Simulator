d497591c9ba8b0c457d7a6abb449b34716c6db2c
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-25 14:04:07 +0530

    Update tempSIMUL_I_TYPE.py

--- tempSIMUL_I_TYPE.py
+++ tempSIMUL_I_TYPE.py
@@ -7,19 +7,23 @@ def I_TYPE( line ):
   rd = line[20:25]
   opcode = line[25:32]
 
+  #lw
   if opcode == '0000011':
   #rd = mem(rs1 + sext(imm[11:0]))
     register[rd] = memory[register[rs1] + bin_to_dec(imm)]
-  
+
+  #addi
   if opcode == '0010011':
     if funct3 == '000':
       register[rd] = register[rs] + bin_to_dec(imm)
-    
+
+  #sltiu
     elif funct3 == '011':
       #rd = 1. If unsigned(rs) < unsigned(imm)
       if abs(register[rs]) < abs(bin_to_dec(imm)) :
         register[rd] = 1   
 
+  #jalr
   if opcode == '1100111':
     register[rd] = PC + 4
     #And store(link) the return address in (rd).
