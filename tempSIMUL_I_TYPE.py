3022353aaaa87040a129c2a7b46fd0b398d5caf6
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-03-25 13:49:08 +0530

    Create tempSIMUL_I_TYPE.py

---
+++ tempSIMUL_I_TYPE.py
@@ -0,0 +1,30 @@
+def I_TYPE( line ):
+  # line is 32 bits
+
+  imm = line[0:12]
+  rs1 = line[12:17]
+  funct3 = line[17:20]
+  rd = line[20:25]
+  opcode = line[25:32]
+
+  if opcode == '0000011':
+  #rd = mem(rs1 + sext(imm[11:0]))
+    register[rd] = memory[register[rs1] + bin_to_dec(imm)]
+  
+  if opcode == '0010011':
+    if funct3 == '000':
+      register[rd] = register[rs] + bin_to_dec(imm)
+    
+    elif funct3 == '011':
+      #rd = 1. If unsigned(rs) < unsigned(imm)
+      if abs(register[rs]) < abs(bin_to_dec(imm)) :
+        register[rd] = 1   
+
+  if opcode == '1100111':
+    register[rd] = PC + 4
+    #And store(link) the return address in (rd).
+    PC = register_name['x6'] + bin_to_dec(imm)
+    if PC%2==1:
+      PC -= 1
+    
+    
