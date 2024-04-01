80961b508680c1eb74c1d4a88e246a3621143528
Mayank Kumar <138568434+MayankK-20@users.noreply.github.com>
2024-04-01 11:57:26 +0530

    Update SIMUL_B_.py

--- SIMULATOR/SIMUL_B_.py
+++ SIMULATOR/SIMUL_B_.py
@@ -1,34 +1,55 @@
 def B_TYPE( line ):
   # line is 32 bits
+  #imm==0?
 
   imm = line[0]+line[24]+line[1:7]+line[20:24]+'0'
   rs1 = line[12:17]
   funct3 = line[17:20]
   rs2 = line[7:12]
-  opcode = line[25::]
 
-  # #beq
-  # if opcode == '0000011':
-  # #rd = mem(rs1 + sext(imm[11:0]))
-  #   register[rd] = memory[register[rs1] + bin_to_dec(imm)]
+  global PC
 
-  # #addi
-  # if opcode == '0010011':
-  #   if funct3 == '000':
-  #     register[rd] = register[rs] + bin_to_dec(imm)
+  #beq
+  if funct3 == '000':
+    if (rs1==rs2):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
 
-  # #sltiu
-  #   elif funct3 == '011':
-  #     #rd = 1. If unsigned(rs) < unsigned(imm)
-  #     if abs(register[rs]) < abs(bin_to_dec(imm)) :
-  #       register[rd] = 1   
+  #bne
+  elif funct3 == '001':
+    if (rs1!=rs2):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
 
-  # #jalr
-  # if opcode == '1100111':
-  #   register[rd] = PC + 4
-  #   #And store(link) the return address in (rd).
-  #   PC = register_name['x6'] + bin_to_dec(imm)
-  #   if PC%2==1:
-  #     PC -= 1
-    
-    
+  #bge
+  elif funct3 == '101':
+    if (bin_to_dec(rs1)>=bin_to_dec(rs2)):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
+
+  #bgeu
+  elif funct3 == '111':
+    if (bin_to_dec(rs1, 'U')>=bin_to_dec(rs2, 'U')):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
+
+  #blt
+  elif funct3 == '100':
+    if (bin_to_dec(rs1)<bin_to_dec(rs2)):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
+
+  #bltu
+  elif funct3 == '110':
+    if (bin_to_dec(rs1, 'U')<bin_to_dec(rs2, 'U')):
+      PC+=bin_to_dec(imm)
+    else:
+      PC+=1
+
+  else:
+    #error
