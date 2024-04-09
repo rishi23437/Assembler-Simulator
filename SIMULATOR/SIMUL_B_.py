782215987c67ec0e9c9be85b69e6d1bf12985e5b
saanvi23454 <160488729+saanvi23454@users.noreply.github.com>
2024-04-09 14:50:13 +0530

    Update SIMUL_B_.py

--- SIMULATOR/SIMUL_B_.py
+++ SIMULATOR/SIMUL_B_.py
@@ -2,19 +2,21 @@ def B_TYPE( line ):
   # line is 32 bits
   #imm==0?
 
+  global PC
+  global register
+  global memory
+
   imm = line[0]+line[24]+line[1:7]+line[20:24]+'0'
   rs1 = line[12:17]
   funct3 = line[17:20]
   rs2 = line[7:12]
 
-  global PC
-
   #beq
   if funct3 == '000':
-    if (rs1==rs2):
-      PC+=bin_to_dec(imm)
+    if (register[rs1] == register[rs2]):
+      PC = PC + bin_to_dec(imm)
     else:
-      PC+=1
+      PC += 4
 
   #bne
   elif funct3 == '001':
