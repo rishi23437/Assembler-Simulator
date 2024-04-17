fd6a960af9cc112a729ae0425df55b1e1957c3b0
Rishi <162436203+rishi23437@users.noreply.github.com>
2024-04-17 13:28:10 +0530

    Update Bonus_Simulator.py

--- Bonus_Simulator.py
+++ Bonus_Simulator.py
@@ -21,6 +21,34 @@ def rvrs( line ):
   PC += 4
 ####################################################
 
+def mul(line):
+    '''
+    Input format: Filler bits(10 zeroes) + rd + rs1 + rs2 + opcode(0000100)
+    '''
+    global PC, register
+
+    rd = line[10:15]
+    rs1 = line[15:20]
+    rs2 = line[20:25]
+
+    register[rd] = mul_bin(register[rs1], register[rs2])
+
+    PC += 4
+
+def rst():
+    '''
+    No input for reset, just check in while loop if opcode matches with 0000101
+    '''
+    global PC, register
+    for key in register:
+        if key == '00010':
+            register[key] = '00000000000000000000000100000000'
+            continue
+        else:
+            register[key] = '00000000000000000000000000000000'
+
+    PC += 4
+
 
 
 
